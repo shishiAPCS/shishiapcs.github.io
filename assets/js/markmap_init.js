882fc1388e84b3eadd61f5aa201731e1af28bbe1
shishiAPCS <roy612723+shishi@gmail.com>
2023-04-25 20:36:17 +0800

    Remove the previous Markmap dependencies and the custom script

--- assets/js/markmap_init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const markmapContainers = document.querySelectorAll(".markmap-container");
-  
-    markmapContainers.forEach((container) => {
-      const markdownContent = container.dataset.markdownContent;
-      const markmapInstance = Markmap.create(container, {}, markdownContent);
-    });
-  });
-  
